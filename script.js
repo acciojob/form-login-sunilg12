e114e67811f50a151823c477176ce3af153e4788
Sunil Reddy Gaddam <gaddamsunilreddy20@gmail.com>
2025-12-15 10:52:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function getFormvalue() {
     //Write your code here
+	const fname = document.getElementById("fname").value;
+	const lname = document.getElementById("lnfame").value;
 
+	if(fname === "" || lname === ""){
+		alert("Please enter both FirstName and LastName");
+		return;
+	}
+
+	// /^[A-Za-z\s'-]+$/;
+	const nameRegex =  /^[A-Za-z\s'-]+$/;
+
+	if(!nameRegex.test(fname) || !nameRegex.test(lname)){
+		alert("Name contains only letters, hyphens, spaces and apostrophes");
+		return;
+	}
+	
+	alert(fname+" "+lname);
 }
